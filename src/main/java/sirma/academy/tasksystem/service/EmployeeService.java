26d8964b19e97798d271fea3a5f63d267a5f81e6
Jayed07 <jayed.ws7@gmail.com>
2023-12-17 21:08:09 +0200

    add methods to EmployeeService and ProjectService

--- src/main/java/sirma/academy/tasksystem/service/EmployeeService.java
+++ src/main/java/sirma/academy/tasksystem/service/EmployeeService.java
@@ -1,4 +1,32 @@
 package sirma.academy.tasksystem.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import sirma.academy.tasksystem.model.Employee;
+import sirma.academy.tasksystem.repository.EmployeeRepository;
+
+import java.util.*;
+
+@Service
 public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public void hireEmployee(Long id) {
+            Employee employee = new Employee();
+            employee.setId(id);
+            employeeRepository.save(employee);
+    }
+
+    public void fireEmployee(Long id) {
+        employeeRepository.findById(id).ifPresent(employee -> employeeRepository.delete(employee));
+    }
+
+    public Employee getById(Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    public List<Employee> employees() {
+        return employeeRepository.findAll();
+    }
 }
--- src/main/java/sirma/academy/tasksystem/service/ProjectService.java
+++ src/main/java/sirma/academy/tasksystem/service/ProjectService.java
@@ -1,4 +1,33 @@
 package sirma.academy.tasksystem.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import sirma.academy.tasksystem.model.Project;
+import sirma.academy.tasksystem.repository.ProjectRepository;
+
+import java.util.*;
+
+@Service
 public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public void add (Long id) {
+        Project project = new Project();
+        project.setId(id);
+        projectRepository.save(project);
+    }
+
+    public void delete (Long id) {
+        projectRepository.findById(id).ifPresent(project -> projectRepository.delete(project));
+    }
+
+    public Project getById (Long id) {
+        return projectRepository.findById(id).orElse(null);
+    }
+
+    public List<Project> projects() {
+        return projectRepository.findAll();
+    }
 }
