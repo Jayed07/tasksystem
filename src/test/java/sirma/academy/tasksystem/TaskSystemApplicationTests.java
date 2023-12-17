9aba799d0d993dba4e1933014d13e6745bcf8e4b
Jayed07 <jayed.ws7@gmail.com>
2023-12-17 19:45:31 +0200

    Initial commit

---
+++ src/test/java/sirma/academy/tasksystem/TaskSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package sirma.academy.tasksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
