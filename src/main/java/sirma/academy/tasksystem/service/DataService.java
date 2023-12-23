3718ae6985c7f1c9908013dc6afe97d708a785ad
Jayed07 <jayed.ws7@gmail.com>
2023-12-23 02:44:53 +0200

    add Date Formatter

--- src/main/java/sirma/academy/tasksystem/service/DataService.java
+++ src/main/java/sirma/academy/tasksystem/service/DataService.java
@@ -11,6 +11,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
 @Service
 public class DataService {
 
@@ -50,14 +53,30 @@ public class DataService {
 
                     Long employeeId = Long.parseLong(values[0]);
                     Long projectId = Long.parseLong(values[1]);
-                    LocalDate dateFrom = LocalDate.parse(values[2]);
+                    LocalDate dateFrom = parseDate(values[2]);
+
+                    //Not necessary because of parse method
+//                    if (dateFrom == null) {
+//                        throw new IOException(String.format("Starting date cannot be NULL. Employee: %d; Project: %d", employeeId, projectId));
+//                    }
+//
                     LocalDate dateTo;
                     if (!values[3].equals("NULL")) {
-                        dateTo = LocalDate.parse(values[3]);
+                        dateTo = parseDate(values[3]);
                     } else {
                         dateTo = null;
                     }
 
+                    if (dateTo == null) {
+                        LocalDate dateToTest = LocalDate.now();
+                        if (dateToTest.isBefore(dateFrom)) {
+                            throw new IOException(String.format("End date cannot be before start date. Employee: %d; Project: %d", employeeId, projectId));
+                        }
+                    } else {
+                        if (dateTo.isBefore(dateFrom)) {
+                            throw new IOException(String.format("End date cannot be before start date. Employee: %d; Project: %d", employeeId, projectId));
+                        }
+                    }
 
                     ProjectCard projectCard = new ProjectCard();
                     // Employee
@@ -90,17 +109,16 @@ public class DataService {
                     } else {
                         throw new IOException("Invalid project ID: " + projectId);
                     }
+
                     //Date From
-                    if (dateFrom == null) {
-                        throw new IOException(String.format("Starting date cannot be NULL. Employee: %d; Project: %d", employeeId, projectId));
-                    } else {
-                        projectCard.setDateFrom(dateFrom);
-                    }
+                    projectCard.setDateFrom(dateFrom);
+
                     //Date To
                     projectCard.setDateTo(dateTo);
 
                     //Save project card to repository
                     projectCardService.add(projectCard);
+
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -110,5 +128,16 @@ public class DataService {
         } finally {
             reader.close();
         }
+    }
+
+    public LocalDate parseDate(String date) {
+        DateTimeFormatterBuilder dateTimeFormatterBuilder = new DateTimeFormatterBuilder()
+                .append(DateTimeFormatter.ofPattern("[yyyy-MM-dd]" + "[dd/MM/yyyy]" + "[MM/dd/yyyy]" +
+                        "[dd MMM yyyy]" + "[yyyy/MM/dd]" + "[EEE, MMM dd, yyyy]" + "[yyyy.MM.dd]" +
+                        "[MMM dd, yyyy]" + "[dd-MM-yyyy]" + "[yyyy/MMM/dd]" + "[d MMMM yyyy]" + "[EEE, dd MMM yyyy]"
+                        + "[yy/MM/dd]" + "[MMMM dd, yyyy]" + "[yyMMdd]" + "[d MMM yy]" + "[dd/MMM/yyyy]" + "[yyyy/MMM/d]"
+                        + "[dd-MMM-yyyy]" + "[EEE, yyyy/MM/dd]"));
+        DateTimeFormatter dateTimeFormatter = dateTimeFormatterBuilder.toFormatter();
+        return LocalDate.parse(date, dateTimeFormatter);
     }
 }
